2ca8d99593a7daf3ae0eb6ffb3fc8aa9cf7aaacc
calzadaharo <calzadaharo@gmail.com>
2026-01-16 16:50:36 +0100

    Same trainings

--- src/main.py
+++ src/main.py
@@ -34,38 +34,51 @@ X = lf.select(categorical_columns + numerical_columns).collect().to_numpy()
 y = lf.select(target_column).collect().to_numpy().ravel()
 
 
-def objective(trial, X, y):
-    n_estimators = trial.suggest_int("n_estimators", 100, 1000)
-    max_depth = trial.suggest_int("max_depth", 3, 10)
-    min_samples_split = trial.suggest_int("min_samples_split", 2, 10)
-    min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 10)
-    max_features = trial.suggest_float("max_features", 0.1, 1.0)
-    criterion = trial.suggest_categorical("criterion", ["gini", "entropy"])
-
+init_time = time.time()
+for i in range(100):
     model = RandomForestClassifier(
-        n_estimators=n_estimators,
-        max_depth=max_depth,
-        min_samples_split=min_samples_split,
-        min_samples_leaf=min_samples_leaf,
-        max_features=max_features,
-        criterion=criterion,
+        n_estimators=1000,
+        max_depth=10,
         random_state=42,
         n_jobs=-1,
     )
-
-    model.fit(X, y)
-
-    score = cross_val_score(model, X, y, cv=5).mean()
-
-    return score
-
-
-study = optuna.create_study(direction="maximize")
-
-start_time = time.time()
-study.optimize(lambda trial: objective(trial, X, y), n_trials=100)
+    model.cross_val_score(X, y, cv=5)
 end_time = time.time()
-print(f"Time taken: {end_time - start_time} seconds")
+print(f"Time taken training 100 models: {end_time - init_time} seconds")
 
-print(study.best_trial.params)
-print(study.best_trial.value)
+
+# def objective(trial, X, y):
+#     n_estimators = trial.suggest_int("n_estimators", 100, 1000)
+#     max_depth = trial.suggest_int("max_depth", 3, 10)
+#     min_samples_split = trial.suggest_int("min_samples_split", 2, 10)
+#     min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 10)
+#     max_features = trial.suggest_float("max_features", 0.1, 1.0)
+#     criterion = trial.suggest_categorical("criterion", ["gini", "entropy"])
+
+#     model = RandomForestClassifier(
+#         n_estimators=n_estimators,
+#         max_depth=max_depth,
+#         min_samples_split=min_samples_split,
+#         min_samples_leaf=min_samples_leaf,
+#         max_features=max_features,
+#         criterion=criterion,
+#         random_state=42,
+#         n_jobs=-1,
+#     )
+
+#     model.fit(X, y)
+
+#     score = cross_val_score(model, X, y, cv=5).mean()
+
+#     return score
+
+
+# study = optuna.create_study(direction="maximize")
+
+# start_time = time.time()
+# study.optimize(lambda trial: objective(trial, X, y), n_trials=100)
+# end_time = time.time()
+# print(f"Time taken: {end_time - start_time} seconds")
+
+# print(study.best_trial.params)
+# print(study.best_trial.value)
